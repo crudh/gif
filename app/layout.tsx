502ab33752f1fc4dabb5ac57077e17bf9255bc8e
Christian Rudh <christian@rudh.se>
2023-03-17 14:44:06 +0100

    Add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { Search } from "../src/components/Search";
 import "./globals.css";
 
@@ -8,12 +9,15 @@ export const metadata = {
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en" className="bg-black text-white">
+    <html lang="en" className="text-white bg-black">
       <body className="p-4">
         <div className="pb-4">
           <Search />
         </div>
         <div>{children}</div>
+        <div className="flex justify-center pt-8 pb-2">
+          <img src="/giphy.png" alt="Powered by Giphy" />
+        </div>
       </body>
     </html>
   );
