86953d70bbbf15330b09c74ecb6fa677bac2f570
Christian Rudh <christian@rudh.se>
2023-03-20 08:12:24 +0100

    Remove click load and always show clipboard (#1)

--- src/components/GifPreview.tsx
+++ src/components/GifPreview.tsx
@@ -1,52 +1,16 @@
-"use client";
 /* eslint-disable @next/next/no-img-element */
-import { useState } from "react";
-import { ClipboardIcon } from "../icons/ClipboardIcon";
 import { Gif } from "../../types/Gif";
+import { GifClipboard } from "./GifClipboard";
 
 export const GifPreview = ({ gif }: { gif: Gif }) => {
-  const [src, setSrc] = useState(gif.previewUrl);
-  const [copied, setCopied] = useState(false);
-
-  const handleClick = () => {
-    setSrc(gif.url);
-  };
-
-  const handleCopyUrl = () => {
-    navigator.clipboard
-      .writeText(gif.url)
-      .then(() => {
-        setCopied(true);
-        setTimeout(() => setCopied(false), 1000);
-      })
-      .catch(() => console.error("Failed to copy url"));
-  };
-
-  const isPreview = src === gif.previewUrl;
-
-  const conditionalContainerStyles = isPreview
-    ? "opacity-60 hover:ring-2 hover:ring-inset hover:ring-indigo-600"
-    : "ring-2 ring-inset ring-yellow-400";
-
-  const conditionalClipboardStyles = copied ? "ring-yellow-400" : "ring-black";
-
   return (
     <div
       key={gif.id}
-      onClick={handleClick}
-      className={`flex p-1 relative rounded-lg hover:cursor-pointer transition-opacity ${conditionalContainerStyles}`}
+      className={`flex p-1 relative rounded-lg hover:cursor-pointer transition-opacity`}
     >
-      {!isPreview && (
-        <button title="Copy link" onClick={handleCopyUrl}>
-          <div
-            className={`absolute w-8 h-8 p-1 transition ease-in-out duration-500 bg-black rounded-lg bottom-2 right-2 ring-2 ring-inset ${conditionalClipboardStyles}`}
-          >
-            <ClipboardIcon />
-          </div>
-        </button>
-      )}
+      <GifClipboard gif={gif} />
       <img
-        src={src}
+        src={gif.previewUrl}
         alt={gif.altText ?? ""}
         className="w-full rounded-lg"
         width={gif.width}
