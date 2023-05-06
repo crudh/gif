c1c7c8693680a69a9ff8c621fd199123dc2eaab2
Christian Rudh <christian@rudh.se>
2023-05-06 17:00:20 +0200

    Use observer and server action to load more

--- src/components/LoadMore.tsx
+++ src/components/LoadMore.tsx
@@ -1,21 +1,31 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Gif } from "../../types/Gif";
 import { GifsGrid } from "./GifsGrid";
 import { defaultGifLimit } from "../constants";
+import { useIntersectionObserver } from "../hooks/useIntersectionObserver";
+import { handleSearchGifs } from "../actions";
 
-export const LoadMore = ({
-  onLoadMore,
-}: {
-  onLoadMore: (offset: number) => Promise<Gif[]>;
-}) => {
+export const LoadMore = ({ searchTerm }: { searchTerm: string }) => {
+  const [intersectionRef, isIntersecting] =
+    useIntersectionObserver<HTMLButtonElement>();
   const [moreGifs, setMoreGifs] = useState<Gif[][]>([]);
 
   const handleLoadMore = async () => {
-    const newGifs = await onLoadMore((moreGifs.length + 1) * defaultGifLimit);
-    setMoreGifs([...moreGifs, newGifs]);
+    const gifs = await handleSearchGifs(
+      searchTerm,
+      (moreGifs.length + 1) * defaultGifLimit
+    );
+
+    setMoreGifs([...moreGifs, gifs]);
   };
+
+  useEffect(() => {
+    if (!isIntersecting) return;
+
+    handleLoadMore();
+  }, [isIntersecting]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <>
@@ -23,8 +33,8 @@ export const LoadMore = ({
         <GifsGrid key={index} gifs={gifs} />
       ))}
       <button
+        ref={intersectionRef}
         type="button"
-        onClick={handleLoadMore}
         // className="relative -ml-px inline-flex items-center gap-x-1.5 rounded-r-md px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-gray-300 hover:bg-gray-600"
       >
         Load more
