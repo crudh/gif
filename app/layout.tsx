a6510309509078fd20641f5cc642fc9bfc80e397
Christian Rudh <christian@rudh.se>
2023-05-07 11:46:04 +0200

    Added header and logo (#11)

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,16 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" className="text-white bg-black">
       <body className="p-4">
+        <div className="flex items-center justify-center p-4">
+          <img src="/badger.png" alt="Gif badger" width="100" height="100" />
+          <div className="flex flex-col pl-5">
+            <h1 className="text-4xl">gifs.run</h1>
+            <span className="pt-2 text-xs">
+              because you need a badger with a jetpack to help you find the best
+              gifs optimized for sharing in social media
+            </span>
+          </div>
+        </div>
         <div className="pb-4">
           <Search />
         </div>
