b867083308ca66601fb69c4cd02b7f984cc59f80
Christian Rudh <christian@rudh.se>
2024-02-07 14:20:37 +0100

    Use next image

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import { Search } from "../src/components/Search";
 import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "gifs.run - the fastest way to share gifs",
@@ -13,7 +14,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
     <html lang="en" className="text-white bg-black">
       <body className="p-4">
         <div className="flex items-center justify-center p-4">
-          <img src="/badger.png" alt="Gif badger" width="100" height="100" />
+          <Image src="/badger.png" alt="Gif badger" width="100" height="100" />
           <div className="flex flex-col pl-5">
             <h1 className="text-4xl">gifs.run</h1>
             <span className="pt-2 text-xs">
@@ -27,7 +28,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         </div>
         <div>{children}</div>
         <div className="flex justify-center pt-8 pb-2">
-          <img
+          <Image
             src="/giphy.png"
             alt="Powered by Giphy"
             width="200"
