cb71dba214ebaadd3adf2a978103628eb099b11b
Christian Rudh <christian@rudh.se>
2024-08-20 21:09:12 +0200

    Add deplyEnv constant

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,5 @@ export const tenorApiKey = process.env.TENOR_API_KEY ?? "";
 export const tenorClientKey = process.env.TENOR_CLIENT_KEY ?? "";
 export const tenorBaseUrl =
   process.env.TENOR_BASE_URL ?? "https://tenor.googleapis.com/v2/";
+export const deployEnv: "production" | "preview" | "development" =
+  (process.env.DEPLOY_ENV as any) ?? "local";
