6034159bdf4b0305a1bb26d760842975681baf20
Christian Rudh <christian@rudh.se>
2024-08-22 15:26:17 +0200

    Load manifest and icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,33 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" className="text-white bg-black">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/images/icons/icon-192x192.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          type="image/png"
+          href="/images/icons/icon-192x192.png"
+        />
+        <link
+          rel="preload"
+          href="/fonts/uU9NCBsR6Z2vfE9aq3bh3dSDqFGedA.woff2"
+          as="font"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className="p-4">
         <div className="flex items-center justify-center p-4">
-          <Image src="/badger.png" alt="Gif badger" width="100" height="100" />
+          <Image
+            src="/images/badger.png"
+            alt="Gif badger"
+            width="100"
+            height="100"
+          />
           <div className="flex flex-col pl-5">
             <h1 className="text-4xl">gifs.run</h1>
             <span className="pt-2 text-xs">
@@ -29,7 +53,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         <div>{children}</div>
         <div className="flex justify-center pt-8 pb-2">
           <Image
-            src="/tenor.svg"
+            src="/images/tenor.svg"
             alt="Powered by Tenor"
             width="198"
             height="25"
