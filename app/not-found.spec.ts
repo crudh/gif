18216390704b3c0613bd165fc6710a0bbfd93078
Christian Rudh <christian@rudh.se>
2025-02-20 23:22:04 +0100

    Add 404 page (#61)

---
+++ app/not-found.spec.ts
@@ -0,0 +1,24 @@
+import { expect } from "@playwright/test";
+import { test } from "../src/test/browser/fixtures";
+import { testLayout } from "../src/test/browser/shared";
+
+test("page", async ({ page, baseUrl }) => {
+  await page.goto(`${baseUrl}/dskfjsdkjfk`);
+
+  await testLayout(page);
+
+  const searchInput = page.getByRole("textbox", { name: "search" });
+  await expect(searchInput).toBeEmpty();
+
+  const notFoundImage = page.getByRole("img", { name: "page not found" });
+  await expect(notFoundImage).toBeVisible();
+
+  const homeButton = page.getByRole("link", {
+    name: "Go home",
+  });
+  await expect(homeButton).toBeVisible();
+
+  await homeButton.click();
+
+  await expect(page).toHaveURL(baseUrl);
+});
