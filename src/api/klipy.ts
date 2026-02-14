9c1d3d9234d2374fb560ad47cdd81022506b3d42
Christian Rudh <christian@rudh.se>
2026-02-14 14:08:33 +0100

    Fix sharing event

--- src/api/klipy.ts
+++ src/api/klipy.ts
@@ -98,6 +98,9 @@ export const shareEvent = async (slug: string, searchTerm: string) => {
 
   const response = await fetch(url, {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(body),
   });
   if (!response.ok)
