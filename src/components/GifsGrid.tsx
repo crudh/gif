42c311d1ab0f0bbeb95cd756e0a0f24e8f295808
Christian Rudh <christian@rudh.se>
2026-02-14 14:46:07 +0100

    Update styling of grid
    min height full screen not needed after we use blur previews

--- src/components/GifsGrid.tsx
+++ src/components/GifsGrid.tsx
@@ -1,7 +1,5 @@
 import type { ReactNode } from "react";
 
 export const GifsGrid = ({ children }: { children: ReactNode }) => (
-  <div className="flex flex-wrap justify-center gap-4 min-h-screen">
-    {children}
-  </div>
+  <div className="flex flex-wrap justify-center gap-4">{children}</div>
 );
